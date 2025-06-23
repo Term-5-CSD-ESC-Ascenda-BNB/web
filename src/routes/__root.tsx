0ff3213c91023ddeef69c612a19c527b948aa4cc
ryanteozw <ryanteozw@gmail.com>
2025-06-24 07:35:17 +0800

    style(nav): add basic styling for navbar
    change styling later to match design

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,18 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import styles from './nav.module.css';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <div className={styles.navbar}>
+        <Link to="/" className={styles.link + ' [&.active]:font-bold'}>
           Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        </Link>
+        <Link to="/about" className={styles.link + ' [&.active]:font-bold'}>
           About
         </Link>
-        <Link to="/search" className="[&.active]:font-bold">
+        <Link to="/search" className={styles.link + ' [&.active]:font-bold'}>
           Search
         </Link>
       </div>
