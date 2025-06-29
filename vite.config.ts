c943261db64e3fa19d980d064341aa35508bcb6f
ryanteozw <ryanteozw@gmail.com>
2025-06-29 14:10:43 +0800

    perf: workaround for Tabler icons loading performance

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
       },
     },
     // root: 'src/',
