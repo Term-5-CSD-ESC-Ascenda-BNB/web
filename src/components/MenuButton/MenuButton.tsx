0b3f88cda687b87368d1498a6a35436a2bb27ea8
ryanteozw <ryanteozw@gmail.com>
2025-07-15 02:37:03 +0800

    chore: remove duplicate MenuButton

--- src/components/MenuButton/MenuButton.tsx
+++
@@ -1,8 +0,0 @@
-import { IconDots } from '@tabler/icons-react';
-import { IconButton } from '@/components/IconButton/IconButton';
-import styles from './MenuButton.module.css';
-import React from 'react';
-
-export function MenuButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <IconButton className={styles.menuButton} icon={<IconDots size={20} />} {...props} />;
-}
