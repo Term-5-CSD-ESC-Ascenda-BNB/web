eaa67b3e8614db36d942a364b411376304daceb5
lowronald <ronaldlow10@gmail.com>
2025-07-26 03:44:32 +0800

    Separated Components for Unit Testing (Pt 2)

--- src/types/Room.ts
+++ src/types/Room.ts
@@ -6,3 +6,14 @@ export interface RoomOption {
   price: number;
   totalPrice: number;
 }
+
+export interface Room {
+  name: string;
+  description: string;
+  longDescription: string;
+  options: RoomOption[];
+  images: string[];
+  checkinPolicy?: string;
+  displayPolicy?: string;
+  amenities: string[];
+}
