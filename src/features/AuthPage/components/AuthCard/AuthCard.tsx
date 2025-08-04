8186df28c391728011f869dc46d32ca11539667d
ryanteozw <ryanteozw@gmail.com>
2025-08-04 18:08:38 +0800

    fix: styling on auth pages and workaround for animation not playing

--- src/features/AuthPage/components/AuthCard/AuthCard.tsx
+++ src/features/AuthPage/components/AuthCard/AuthCard.tsx
@@ -12,10 +12,12 @@ export function AuthCard({ title, subtitle, children }: AuthCardProps) {
     <>
       <Paper p={48} radius={'lg'} shadow={'0 2px 6px rgba(0, 0, 0, 0.1)'}>
         <Stack mb={'xl'} gap={0} align="center" justify="center">
-          <Title order={1} ff={theme.other.displayFont} fw={500} fz={'2.5rem'}>
+          <Title order={1} ff={theme.other.displayFont} fw={500} fz={'2.5rem'} ta={'center'}>
             {title}
           </Title>
-          <Text ff={'heading'}>{subtitle}</Text>
+          <Text ff={'heading'} ta={'center'}>
+            {subtitle}
+          </Text>
         </Stack>
 
         {children}
